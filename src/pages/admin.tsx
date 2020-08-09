78e9c661ce8a64694e69db804e15271bf6de28a9
autsada <autsada.t@gmail.com>
2020-08-09 16:31:30 +0700

    Added route protection

--- src/pages/admin.tsx
+++ src/pages/admin.tsx
@@ -1,5 +1,32 @@
+import { useContext, useEffect } from 'react'
+import { useRouter } from 'next/router'
+import Loader from 'react-loader-spinner'
+
 import Admin from '../components/Admin'
+import { AuthContext } from '../context/AuthContextProvider'
+import { isAdmin } from '../helpers/authHelpers'
 
 export default function AdminPage() {
-  return <Admin />
+  const { loggedInUser } = useContext(AuthContext)
+
+  const router = useRouter()
+
+  useEffect(() => {
+    // If user in not authenticated, push to home page
+    if (!loggedInUser) {
+      router.push('/')
+    } else {
+      if (!isAdmin(loggedInUser)) {
+        // Push user to their dashboard
+        router.push('/dashboard')
+        alert('No Authorization.')
+      }
+    }
+  }, [loggedInUser])
+
+  return !isAdmin(loggedInUser) ? (
+    <Loader type='Oval' color='teal' height={30} width={30} timeout={30000} />
+  ) : (
+    <Admin />
+  )
 }
